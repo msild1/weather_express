0b2a4e322514f1a8f69a81231157a3ceb67a4f46
msild1 <markus.sild.001@voco.ee>
2022-12-12 22:48:52 +0200

    Created form request responce with API data

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ const express = require("express")
 const app = express()
 const path = require("path")
 const fetch = require("node-fetch")
+let bodyParser = require('body-parser')
+// const body = require("body-parser")
+let urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 
 app.set("view engine", "ejs")
 app.set("views", path.join(__dirname,"views"))
 
-
-
+const key = "8e7fc1f98df00e5f10dc0e4579b07430"
+let city = "Tartu"
 
 
 app.get("/", function (req,res){
-    let key = "8e7fc1f98df00e5f10dc0e4579b07430"
-    let city = "Tartu"
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
         .then(function(resp){
             return resp.json()
@@ -31,5 +33,27 @@ app.get("/", function (req,res){
             // catch errors
         })
 })
+
+app.post("/",urlencodedParser, function (req,res){
+    let city = req.body.cityname
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+        .then(function(resp){
+            return resp.json()
+        }) // Convert data to json
+        .then(function(data) {
+            let desc = data.weather[0].description
+            let city = data.name
+            let temp = Math.round(parseFloat(data.main.temp)-273.15)
+            res.render("index", {
+                desc: desc,
+                city: city,
+                temp: temp,
+            })
+        })
+        .catch(function(){
+            // catch errors
+        })
+})
+
 console.log("running on http://localhost:3000")
 app.listen(3000)
